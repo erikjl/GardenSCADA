3e0a7ddb4b821af932e53de0cf4a4a7ef183207d
Erik Lee <erikjlee@hotmail.com>
2018-03-28 17:24:02 -0700

    Initial creation of /server

---
+++ server/src/main/java/com/gardenscada/App.java
@@ -0,0 +1,19 @@
+package com.gardenscada;
+
+/**
+ * Main entry point for GardenSCADA server
+ * 3/28/2018 Erik Lee (erikjlee@hotmail.com)
+ *
+ * Alarm class (severity, AlarmAction[])
+ * Sensor class (temp, humidity, composite)
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Starting GardenSCADA server..." );
+        //TODO: set up log
+
+    }
+}
