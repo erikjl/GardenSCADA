80f7f46458a3286d51983af3f4bd5b2b4e158f04
Erik Lee <erikjlee@hotmail.com>
2018-03-29 18:07:36 -0700

    Work on main()

--- server/src/main/java/com/gardenscada/App.java
+++ server/src/main/java/com/gardenscada/App.java
@@ -1,8 +1,15 @@
 package com.gardenscada;
 
+import java.io.*;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.Properties;
+import java.util.Hashtable;
+
 /**
  * Main entry point for GardenSCADA server
- * 3/28/2018 Erik Lee (erikjlee@hotmail.com)
+ * @version 1.0
+ * @author Erik Lee (erikjlee@hotmail.com)
  *
  * Alarm class (severity, AlarmAction[])
  * Sensor class (temp, humidity, composite)
@@ -10,10 +17,53 @@ package com.gardenscada;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Starting GardenSCADA server..." );
-        //TODO: set up log
+        Properties appProps = new Properties();
+        //String dir = System.getProperty("user.dir");
+        String currDir = new File(".").getAbsolutePath() ;
+        System.out.println("Current directory: " + currDir);
+
+        try {
+            FileInputStream in = new FileInputStream("properties");
+            appProps.load(in);
+        }
+        catch(IOException f) {
+            System.out.println("Properties file 'properties' not found. Exiting...");
+            return;
+        }
+
+        Logger log = LogManager.getLogManager().getLogger("MAIN");
+
+        //Any command args?
+        for(String arg:args) {
+
+        }
+
+        //Handle console input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            System.out.print("[GSCADA]: ");
+            String input = br.readLine();
+            //is it a property? if so, print it out
+            //TODO: how to update property?
+
+            switch(String input.toUpperCase())
+            {
+                case "EXIT":
+                    //TODO: clean up, save Peoperties
+                    break;
+
+                default:
+
+            }
+            System.out.println("IN: " + input);
+        }
 
     }
 }
+
+
+
+
